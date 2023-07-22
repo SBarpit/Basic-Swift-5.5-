28769ffe0fd295539f92c29390134b2d66bb1fe5
Arpit Srivastava <arpit9840@gmail.com>
2023-07-22 18:02:01 +0530

    Async await example

--- Asyn-await.playground/Pages/Async In advance.xcplaygroundpage/Contents.swift
+++ Asyn-await.playground/Pages/Async In advance.xcplaygroundpage/Contents.swift
@@ -9,18 +9,25 @@ import PlaygroundSupport
 PlaygroundPage.current.needsIndefiniteExecution = true
 
 
+func synFunc() -> [Double] {
+    (1...10).map { _ in Double.random(in: -10...30) }
+}
+
 func fetchWeatherHistory() async -> [Double] {
-    (1...100_000).map { _ in Double.random(in: -10...30) }
+    print("1")
+    return (1...100_000).map { _ in Double.random(in: -10...30) }
 }
 
 func calculateAverageTemperature(for records: [Double]) async -> Double {
     let total = records.reduce(0, +)
     let average = total / Double(records.count)
+    print("2")
     return average
 }
 
 func upload(result: Double) async -> String {
-    "OK"
+    print("3")
+    return "OK"
 }
 
 func processWeather() async {
@@ -30,5 +37,8 @@ func processWeather() async {
     print("Server response: \(response)")
 }
 
+print(synFunc())
 await processWeather()
+await fetchWeatherHistory()
+print(synFunc())
 PlaygroundPage.current.finishExecution()
